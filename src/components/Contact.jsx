d640d91be40aea84e5b2ba86dedb49de545bb8a0
Anjishnu Kumar <anjishnukumar@Anjishnus-MacBook-Air.local>
2023-06-10 08:45:01 +0530

    first commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='contact'>
+      <main>
+        <h1>Contact Us</h1>
+        <form>
+            <div>
+                <label>Name</label>
+                <input type="text" placeholder="Enter your name" required/>
+            </div>
+
+            <div>
+                <label>Email</label>
+                <input type="email" placeholder="xyz@gmail.com" required/>
+            </div>
+
+            <div>
+                <label>Message</label>
+                <input type="text" placeholder="Tell us about your query" required/>
+            </div>
+
+            <button type="submit">Send</button>
+
+        </form>
+      </main>
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>TechyStar</h1>
+            <p>Made with &hearts; by Anjishnu Kumar</p>
+        </div>
+
+        <div>
+            <h5>Follow Us</h5>
+            
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
